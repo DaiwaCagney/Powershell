52609aa0e5f241c3eeff97e116cbdf0e75450864
unknown <hoyuysh@gmail.com>
2024-08-23 00:35:42 +0800

    Change CheckAdmin

--- CheckAdmin.ps1
+++ CheckAdmin.ps1
@@ -4,3 +4,10 @@ if (!$checkadmin.IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator"
 } else {
     Write-Host "Is Administrator" -ForegroundColor Green
 }
+
+$checkadmin = [Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()
+if (-Not $checkadmin.IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
+    $CMD = $MyInvocation.Line
+    $CMDARGS = $CMD.Substring($CMD.IndexOf('.ps1')+4)
+    Start-Process Powershell -Verb RunAs -ArgumentList "$PSCommandPath $CMDARGS"
+}
